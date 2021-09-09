f7241a96e66d2eb19e83c01af771b20d4134677c
ListerineBoi <adrianstefano99@gmail.com>
2021-09-09 21:13:50 +0700

    second

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/pindah_memilih', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/pindah_memilih/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/pindah_memilih/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
